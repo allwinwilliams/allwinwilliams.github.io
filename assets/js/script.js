1ef2d77b8b7625bd1befb398132a82dd31cc6151
Allwin Williams <allwinwilliams.info@gmail.com>
2020-12-21 09:54:51 +0530

    ui animations fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
+function show_modal(url){
+  let modal_object = $('<object width="100%" height="100%" />');
+  modal_object.attr("data", url);
+  $('#modal-body-space').html(modal_object);
+  $('#projectModal').modal('show');
+}
 
 $(document).ready(function() {
-  function show_modal(url){
-    let modal_object = $('<object width="100%" height="100%" />');
-    modal_object.attr("data", url);
-    $('#modal-body-space').html(modal_object);
-    $('#projectModal').modal('show');
-  }
 
   // function scrollTo(section){
   // 	document.querySelector(section).scrollIntoView({
@@ -24,16 +24,27 @@ $(document).ready(function() {
     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
   }
   // If element is scrolled into view, fade it in
-  $('.scroll-animations.animated')
+
   $(window).scroll(function() {
     console.log("scrolling");
     $('.scroll-animations.animated').each(function() {
-      if (isScrolledIntoView(this) === true) {
+      if (isScrolledIntoView(this) === true && $(this).css('opacity') != 1) {
         $(this).css('opacity', '1');
-        console.log("yessss");
         $(this).addClass('animate__animated animate__bounceInUp');
-
+        $(this).bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+          function(){
+            $(this).removeClass('animate__animated animate__bounceInUp');
+          });
       }
     });
   });
+  $('.hover-animations').hover(function(){
+    console.log("hovered");
+    console.log(this);
+    $(this).addClass('animate__animated animate__headShake');
+    $(this).bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+      function(){
+        $(this).removeClass('animate__animated animate__headShake');
+      });
+  })
 });
