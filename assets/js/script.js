b899446a5d545cd4a26fee3851732a8895a5bc86
Allwin Williams <allwinwilliams.info@gmail.com>
2021-01-06 10:32:48 +0530

    project ui changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,7 @@ $(document).ready(function() {
     });
   });
   $('.hover-animations').hover(function(){
+    $(this).removeClass('animate__animated animate__fadeInUp');
     $(this).addClass('animate__animated animate__headShake');
     $(this).bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
       function(){
@@ -54,6 +55,7 @@ $(document).ready(function() {
   $('#more-projects').click(function(){
     $('.hidden-project').removeClass('d-none');
     $('.hidden-project').addClass('animate__animated animate__fadeInUp');
+
     $(this).addClass('d-none');
   });
 });
